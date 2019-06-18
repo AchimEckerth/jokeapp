063b9742aad167774278b48f732a51877877aac1
AchimEckerth <46998663+AchimEckerth@users.noreply.github.com>
2019-06-18 15:17:01 +0200

    Add files via upload

--- action-app_joke_tuto.py
+++ action-app_joke_tuto.py
@@ -2,10 +2,10 @@ from hermes_python.hermes import Hermes
 
 
 def action_wrapper(hermes, intent_message):
-    result_sentence = 'How are you'
+    result_sentence = 'Wie geht es dir?'
     hermes.publish_end_session(intentMessage.session_id, result_sentence)
 
 
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
-        h.subscribe_intent("snips-qOPRiLbJKW:askJoke", action_wrapper).start()
+        h.subscribe_intent("CrystalMethod:hello", action_wrapper).start()
