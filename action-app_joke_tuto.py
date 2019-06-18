348fe90a10db719a939ac1770b4673a95ba37b88
AchimEckerth <46998663+AchimEckerth@users.noreply.github.com>
2019-06-18 15:57:06 +0200

    Add files via upload

--- action-app_joke_tuto.py
+++ action-app_joke_tuto.py
@@ -3,7 +3,7 @@ from hermes_python.hermes import Hermes
 
 def action_wrapper(hermes, intent_message):
     result_sentence = 'Wie geht es dir?'
-    hermes.publish_end_session(intentMessage.session_id, result_sentence)
+    hermes.publish_end_session(intent_message.session_id, result_sentence)
 
 with Hermes("localhost:1883") as h:
     h.subscribe_intent("snips-qOPRiLbJKW:Hallo", action_wrapper).start()
